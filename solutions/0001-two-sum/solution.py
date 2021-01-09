7b05330a00971712aa9ea325812879b40e01c8aa
Kyuwon Lee <justinklee345@gmail.com>
2021-01-09 10:14:42 +0000

    Sync LeetCode submission Runtime - 48 ms (32.73%), Memory - 14.6 MB (100.00%)

---
+++ solutions/0001-two-sum/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for x in range(len(nums)):
+            for y in range(x+1, len(nums)):
+                if nums[x] + nums[y] == target:
+                    List = []
+                    List.append(x)
+                    List.append(y)
+                    return List
