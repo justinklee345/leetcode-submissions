c7b4a7e0e7f6652eb7311a5f60383e320d31c825
Kyuwon Lee <justinklee345@gmail.com>
2022-09-14 21:03:46 +0000

    Sync LeetCode submission Runtime - 5928 ms (5.00%), Memory - 14.9 MB (100.00%)

--- solutions/0001-two-sum/solution.py
+++ solutions/0001-two-sum/solution.py
@@ -1,9 +1,15 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for x in range(len(nums)):
-            for y in range(x+1, len(nums)):
-                if nums[x] + nums[y] == target:
-                    List = []
-                    List.append(x)
-                    List.append(y)
-                    return List
+        # loop through the list and create a dictionary that maps the value to a tuple of its idx and the target - the key value
+        # targetMap = {}
+        # for idx, val in enumerate(nums):
+        #     targetMap.setdefault(val, (idx, target - val))
+        
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if (nums[i] + nums[j] == target):
+                    return [i, j]
+        return [-1, -1]
+            
+        
+        
